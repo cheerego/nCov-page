be60925348ba0fc798d4bb2c164c85f8e7a8cbc2
jenswang <wang.junjie@pku.edu.cn>
2020-02-27 16:47:09 +0800

    repush

--- dashboard/src/index.js
+++ dashboard/src/index.js
@@ -21,16 +21,17 @@ module.exports = new Vue({
     },
     this.chartSettings = {
       labelMap:{
-        'confirm': '确诊人数',
-        'heal': '治愈人数',
-        'suspect':'新增人数'
+        'confirmedNum': '确诊人数',
+        'curesNum': '治愈人数',
+        'suspectedNum':'新增人数',
+	'deathsNum':'死亡人数'
       }
     }
     return {
       message: 'Click me!',
       isVisible: true,
       chartData: {
-        columns: ['create_time','confirm','suspect','heal'],
+        columns: ['date','confirmedNum','suspectedNum','curesNum','deathsNum'],
         rows: []
       }
     }
@@ -45,14 +46,16 @@ module.exports = new Vue({
       const response = await fetch(window.env.apiUrlSZ)
       const result = await response.json()
       // handle chart data
-      const rows = result.data.items;
+      const rows = result.data;
       // add new patient
+/*
       for (var days = 0; days < rows.length-1; days++){
         rows[days].suspect = rows[days].confirm-rows[days+1].confirm;
       }
       rows.sort((a, b) => {
         return new Date(a.create_time) - new Date(b.create_time);
       })
+*/
       this.chartData.rows = rows
     }
   }
